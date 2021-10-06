da14390a1f70097ed4b3f64ab376bef874306515
gsilvaro11 <guiuser1999@gmail.com>
2021-10-06 13:24:47 -0300

    Arrumando exercicio 06

--- src/com/dbc/Exercicio/Exercicio06.java
+++ src/com/dbc/Exercicio/Exercicio06.java
@@ -1,25 +1,29 @@
 package com.dbc.Exercicio;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String lista[] = new String[] {"CACHORRO", "DOG", "TEMPO", "TIME"};
+        String lista[] = new String[] {"CACHORRO", "DOG", "TEMPO", "TIME",
+                "AMOR", "LOVE", "CIDADE", "CITY", "FELIZ", "HAPPY"};
 
         System.out.println("Digite o que gostaria de traduzir: ");
         String digitado = scanner.nextLine();
 
-//        for(int i = 0; i < lista.length; i++){
-//            if(digitado.equals(lista[i])){
-//                int teste = i % 2 = 0 ? i -1 : + 1;
-//                
-//                break;
-//            }else{
-//                continue;
-//            }
-//        }
+        for(int i = 0; i < lista.length; i++){
+            if(digitado.toUpperCase().equals(lista[i])){
+                int teste = i % 2 == 0 ? i + 1 : i - 1;
+                System.out.printf("Palavra digitada: %s\nTradução: %s"
+                , digitado.toUpperCase(), lista[teste]);
+                break;
+            }else{
+                continue;
+            }
+        }
     }
 }
