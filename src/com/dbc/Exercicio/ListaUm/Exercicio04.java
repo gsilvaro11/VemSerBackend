47bd4655381c3f9e08ae74b5bfee8958881adb4f
gsilvaro11 <guiuser1999@gmail.com>
2021-10-07 13:07:44 -0300

    Adicioanando exercicio04 lista 1

--- src/com/dbc/Exercicio/ListaUm/Exercicio04.java
+++ src/com/dbc/Exercicio/ListaUm/Exercicio04.java
@@ -1,11 +1,58 @@
 package com.dbc.Exercicio.ListaUm;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio04 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] estados = {"Rio Grande do Sul", "São Paulo", "Rio Janeiro",};
 
+        for (int i = 0; i < estados.length; i++) {
+            System.out.printf("%s   %s\n", estados[i], i + 1);        }
+
+
+        System.out.println("Digite o index do estado");
+        int indexEscolhido = scanner.nextInt();
+
+        switch (indexEscolhido){
+            case 1:
+                System.out.println("p - Porto Alegre, v - Viamão");
+                break;
+            case 2:
+                System.out.println("s - São Paulo, sa - Bauru");
+                break;
+
+            case 3:
+                System.out.println("r - Rio de Janeiro, n - Niteroi");
+                break;
+
+            default:
+                System.out.println("Opção inválida");
+        }
+
+        char escolhaCidade = scanner.next().charAt(0);
+        switch (escolhaCidade){
+            case 'p':
+                System.out.println("Porto Alegre\nPopulaçao: 1milhão\nIDH: 7\nPonto Turistico: Mercadao");
+                break;
+            case 'v':
+                System.out.println("Viamão\nPopulaçao: 500mil\nIDH: 7\nPonto Turistico: Santa Cicilia");
+                break;
+            case 's':
+                System.out.println("São Paulo\nPopulaçao: 10milhões\nIDH: 8\nPonto Turistico: Augusta");
+                break;
+            case 'b':
+                System.out.println("Bauru\nPopulaçao: 3milhão\nIDH: 8\nPonto Turistico: Estadio");
+                break;
+            case 'r':
+                System.out.println("Rio de Janeiro \nPopulaçao: 7milhão\nIDH: 7\n:Ponto Turistico: Cristo");
+                break;
+            case 'n':
+                System.out.println("Niteroi \nPopulaçao: 1milhão\nIDH: 7\nPonto Turistico: Praia");
+                break;
+            default:
+                System.err.println("Opção inválida");
+
+        }
     }
 }
