a8ac6c39605c62117495297aef34422f22fab759
Ne Guilherme dai Silva <nei.guilherme@dbccompany.com.br>
2021-11-26 10:28:04 -0300

    Adicionando homework mongodb

--- src/com/dbc/moduloCinco/src/main/java/main.java
+++ src/com/dbc/moduloCinco/src/main/java/main.java
@@ -1,11 +1,18 @@
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.Arrays;
+
 
 public class main {
     public static void main(String[] args) {
@@ -26,11 +33,24 @@ public class main {
 
         homework02.insertOne(novoProduto);
 
+
+        Bson updates = Updates.combine(
+                Updates.set("valorUnitario", 1),
+                Updates.set("Descricao", "Guarana"),
+                Updates.set("tipoProduto", "BEBIDA"));
+
+        UpdateOptions options = new UpdateOptions().upsert(true);
+        UpdateResult result2 = homework02.updateOne(novoProduto, updates, options);
+
+        // deleteOne
+        homework02.deleteOne(Filters.eq("_id", 14));
+      
         //Retona os valores
         Document home2 = homework02.find().first();
         System.out.println(home2);
 
         client.close();
     }
+
 
 }
